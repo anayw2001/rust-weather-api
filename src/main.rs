7f484ddc1fef3d8cdc7918566bebb97de0de8d52
Anay Wadhera <awadhera@berkeley.edu>
2022-11-05 00:04:49 -0400

    weather-api: Handle longitude/longitude endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[get("/v1/api/{latitude}/{longitude}")]
+async fn parse_lat_long(lat_long: web::Path<(f64, f64)>) -> impl Responder {
+    let lat = lat_long.0;
+    let long = lat_long.1;
+    format!("Latitude: {lat}, Longitude {long}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+            .service(parse_lat_long)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
