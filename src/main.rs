76d401c7fae2f66492fb6672c90d60bc912fbe53
Anay Wadhera <awadhera@berkeley.edu>
2022-11-05 10:16:11 -0400

    Add support for deserializing credentials json
    Lay out logic for query in parse_lat_long
    Fix math build
    Introduce database utils

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+mod database_utils;
+mod math;
+
 use actix_web::{get, web, App, HttpServer, Responder};
+use serde::Deserialize;
+use std::fs;
+
+struct Location {
+    latitude: f64,
+    longitude: f64,
+}
+
+#[derive(Deserialize, Debug)]
+struct APIKey {
+    key: String,
+}
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
@@ -9,11 +24,20 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 async fn parse_lat_long(lat_long: web::Path<(f64, f64)>) -> impl Responder {
     let lat = lat_long.0;
     let long = lat_long.1;
+    // figure out a better way to find a location in the database that is more efficient than looping over the entire db
+    // anyway figure out if the db contains a location within 2km of the received lat/long using `math::haversine`
+    // if contains, format the json with the relevant entry from the db.
+    // if not, query owm and store the result of the api call in the db, then return the information
+    // the client needs.
     format!("Latitude: {lat}, Longitude {long}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Load openweathermap api key.
+    let owm_credentials_raw = fs::read_to_string("creds.json").expect("No creds.json file found.");
+    let owm_key: APIKey = serde_json::from_str(&owm_credentials_raw).expect("bad json");
+    println!("owm_api_key: {owm_key:?}");
     HttpServer::new(|| {
         App::new()
             .route("/hello", web::get().to(|| async { "Hello World!" }))
--- src/math.rs
+++ src/math.rs
@@ -1,10 +1,10 @@
 use crate::Location;
 
-const EARTH_RADIUS_KM: i32 = 6371;
+const EARTH_RADIUS_KM: f64 = 6371_f64;
 
 /// Returns distance in kilometeres between location1 and location2, which represent
 /// latitude and longitude coordinates of two points on Earth.
-pub(crate) fn haversine(location1: Location, location2: Location) -> i32 {
+pub(crate) fn haversine(location1: Location, location2: Location) -> f64 {
     let location1_lat_radians = location1.latitude.to_radians();
     let location2_lat_radians = location2.longitude.to_radians();
     let location_lat_diff_radians = (location2.latitude - location1.latitude).to_radians();
@@ -15,6 +15,6 @@ pub(crate) fn haversine(location1: Location, location2: Location) -> i32 {
             * (location_long_diff_radians / 2.0).sin().powi(2);
     let central_angle = 2.0 * central_angle_inner.sqrt().asin();
 
-    let distance = earth_radius_kilometer * central_angle;
+    let distance = EARTH_RADIUS_KM * central_angle;
     distance
 }
